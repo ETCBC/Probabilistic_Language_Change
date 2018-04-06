dff60e9e0e538873392a5127c87d54740c258e16
etiennevandebijl <etiennevandebijl@hotmail.com>
2018-04-06 14:46:09 +0200

    Clustering py

---
+++ etienne_NB/Clustering analysis.py
@@ -0,0 +1,30 @@
+from scipy.cluster.hierarchy import dendrogram, linkage
+import matplotlib.pyplot as plt
+from scipy.spatial.distance import squareform
+
+clusterMethods = {"single","complete","average","weighted","median","centroid","ward"}
+
+#Hierarchical Clustering
+def dendogramPlot(linkageMatrix, labels):
+    # Input: linkage Matrix, labels for the objects.
+    # Output: dendogram plot
+    plt.figure(figsize=(25,10))
+    dendrogram(Z, labels = books)
+    plt.show()
+    
+def linkageFunction(y, method):
+    # Input: Y: Condensed distance matrix y. N rows and 2 columns. 
+    # Method: Single, Complete, Average, Weighted, Ward, Median, Centroid
+    # Output: Hierarchical clustering encoded as a linkage matrix
+    return linkage(y, method)
+
+def squareFormFunction(v):
+    # Input: Either a condensed or redundant distance matrix
+    # Ouput: If a condensed distance matrix is passed, a redundant one is returned.
+    # if a redundant one is passed, a condensed distance matrix is returned.
+    return squareform(v)
+
+
+
+
+
