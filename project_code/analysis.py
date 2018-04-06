657a1a1c02b32e9142871f54fb9e0f1417c62442
Cody Kingham <codyakingham@gmail.com>
2018-04-06 14:28:08 +0200

    new dir structures

--- project_code/analysis.py
+++
@@ -1,7 +0,0 @@
-'''
-This module contains code used to analyze the BHSA data.
-The analysis includes functions which count basic features
-in the data, construct bigrams for markov chain transitions, 
-and visualize the results. 
-'''
-
